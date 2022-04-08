a914ece1df4ae28dff04ddeff43d5f38d803ec94
Joe Makepeace <joe.makepeace01@gmail.com>
2022-04-08 18:28:53 +0100

    Added unique track add to playlist and removed the track removal function as its highly abusable

--- README.md
+++ README.md
@@ -13,5 +13,26 @@ the master spotify playlist.
 
 ## Usage
 
+### Authorization
+
+On the first run, a url will show which you authorize this discord bot with for the
+spotify api. Then take the code which shows up and change the auth code in .env.
+Afterwards, this will be refreshed using the refresh_token and then you won't
+have to authorize this app again (hopefully :/, otherwise you will need to do
+this process again).
+
+### .env file settings
+
+```
+DISCORD_TOKEN=<DISCORD_API_TOKEN>
+DISCORD_GUILD=<DISCORD_SERVER_NAME>
+DISCORD_RHYTHM_CHANNEL_ID=<CHANNEL_WHERE_PEOPLE_CAN_VOTE_ON_SONGS_TO_ADD>
+SPOTIFY_CLIENT_ID=<SPOTIFY_API_CLIENT_ID>
+SPOTIFY_PLAYLIST_ID=<SPOTIFY_PLAYLIST_ID_TO_ADD_SONGS_TO>
+VOTER_ROLE_ID=<DISCORD_SERVER_ROLE_SETTINGS_ROLE_ID>
+```
+
+### Deployment
+
 Recommended way to deploy this bot is to create a free-tier VM
 on Azure/GCP/AWS and host it as a NodeJS server.
--- index.js
+++ index.js
@@ -3,10 +3,20 @@ const { Client, Intents, Emoji } = require("discord.js");
 
 const ytdl = require('ytdl-core');
 const SpotifyWebApi = require('spotify-web-api-node');
+const scopes = [
+        "playlist-modify-public",
+        "playlist-modify-public"
+      ],
+      redirectUri = 'http://www.example.com/callback',
+      clientId = process.env.SPOTIFY_CLIENT_ID,
+      state = "ello";
 const spotifyApi = new SpotifyWebApi({
-    clientId: process.env.SPOTIFY_CLIENT_ID,
-    clientSecret: process.env.SPOTIFY_CLIENT_SECRET
+    clientId: clientId,
+    clientSecret: process.env.SPOTIFY_CLIENT_SECRET,
+    redirectUri: redirectUri
 });
+const authorizeUrl = spotifyApi.createAuthorizeURL(scopes, state);
+console.log(authorizeUrl);
 
 const client = new Client({
 	intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_MESSAGE_REACTIONS],
@@ -33,9 +43,50 @@ client.on("messageReactionAdd", async (reaction, user) => {
                 const voterRole = process.env.VOTER_ROLE_ID;
                 if (roles.cache.has(voterRole)) {
                     if (userMsg.includes("open.spotify.com")) {
+                        // Get track scheme
                         let track_id = userMsg.split("/track/")[1].split("?")[0];
                         let track_scheme = `spotify:track:${track_id}`;
                         console.log("Adding:", track_scheme, "to playlist...");
+                        
+                        // The code that's returned as a query parameter to the redirect URI
+                        const authCode = process.env.AUTH_CODE;
+
+                        // Add to playlist using Spotify Web API
+                        spotifyApi.authorizationCodeGrant(authCode).then(
+                            data => {
+                                // Get access token
+                                console.log("Access token is:", data.body["access_token"]);
+                                console.log("Refresh token is:", data.body["refresh_token"]);
+                                console.log("Expires in:", data.body["expires_in"]);
+
+                                spotifyApi.setAccessToken(data.body["access_token"]);
+                                spotifyApi.setRefreshToken(data.body["refresh_token"]);
+                                
+                                // Update playlist if access_token hasn't expired
+                                addPlaylist(track_scheme);
+                                channel.send(
+                                    `Track added to playlist: https://open.spotify.com/playlist/${process.env.SPOTIFY_PLAYLIST_ID}`);
+                            },
+                            err => {
+                                console.log("Error getting access token:", err);
+                                // clientId, clientSecret and refreshToken has been set on the api object previous to this call.
+                                spotifyApi.refreshAccessToken().then(
+                                    function(data) {
+                                        console.log('The access token has been refreshed!');
+                                    
+                                        // Save the access token so that it's used in future calls
+                                        spotifyApi.setAccessToken(data.body['access_token']);
+                                        
+                                        // Update playlist if access_token has expired but is
+                                        // ... now refreshed (hopefully :/)
+                                        addPlaylist(track_scheme);
+                                    },
+                                    function(err) {
+                                        console.log('Could not refresh access token', err);
+                                    }
+                                );
+                            }
+                        );
                     }
                 }
             }
@@ -43,30 +94,29 @@ client.on("messageReactionAdd", async (reaction, user) => {
     });
 });
 
-client.on("messageReactionRemove", async (reaction, user) => {
-    console.log(user);
-
-    const channel = client.channels.cache.get(process.env.DISCORD_RHYTHM_CHANNEL_ID);
-    const members = Array.from(channel.members);
-    const msgId = reaction.message.id;
-
-    channel.messages.fetch(msgId).then(msg => {
-        const userMsg = msg.content;
-        members.forEach(member => {
-            const userId = member[1].user.id;
-            if (userId == user.id) {
-                const roles = member[1].roles;
-                const voterRole = process.env.VOTER_ROLE_ID;
-                if (roles.cache.has(voterRole)) {
-                    if (userMsg.includes("open.spotify.com")) {
-                        let track_id = userMsg.split("/track/")[1].split("?")[0];
-                        let track_scheme = `spotify:track:${track_id}`;
-                        console.log("Removing:", track_scheme, "to playlist...");
-                    }
-                }
-            }
-        })
+const addPlaylist = track_scheme => {
+    // Get playlist
+    spotifyApi.getPlaylist(process.env.SPOTIFY_PLAYLIST_ID)
+    .then(function(data) {
+        console.log('Some information about this playlist', );
+        const tracks = data.body.tracks.items;
+        let found = false;
+        tracks.forEach(track => {
+            console.log("TRACK:", track);
+            if (track.track.uri == track_scheme) { found = true };
+            console.log("ELLO?:", track.track.uri, track_scheme)
+        });
+        if (!found) {
+            spotifyApi.addTracksToPlaylist(process.env.SPOTIFY_PLAYLIST_ID, [track_scheme])
+            .then(function(data) {
+                console.log('Added tracks to playlist!');
+            }, function(err) {
+                console.log('Something went wrong!', err);
+            });
+        }
+    }, function(err) {
+        console.log('Something went wrong!', err);
     });
-});
+};
 
 client.login(process.env.DISCORD_TOKEN);
